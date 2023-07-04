efbeb2d6acfcaf28534189bb5c0098b81990f727
myluki2000 <lukas636@live.com>
2023-07-04 22:23:21 +0200

    Add query for current user & change name of content field

--- src/main/java/de/unistuttgart/iste/gits/reward/controller/RewardController.java
+++ src/main/java/de/unistuttgart/iste/gits/reward/controller/RewardController.java
@@ -1,10 +1,12 @@
 package de.unistuttgart.iste.gits.reward.controller;
 
+import de.unistuttgart.iste.gits.common.user_handling.LoggedInUser;
 import de.unistuttgart.iste.gits.generated.dto.RewardScores;
 import de.unistuttgart.iste.gits.reward.service.RewardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.ContextValue;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
@@ -19,13 +21,12 @@ public class RewardController {
     private final RewardService rewardService;
 
     @QueryMapping
-    public RewardScores rewardScores(@Argument UUID courseId, @Argument UUID userId) {
-        return rewardService.getRewardScores(courseId, userId);
+    public RewardScores userCourseRewardScores(@Argument UUID courseId, @ContextValue LoggedInUser currentUser) {
+        return rewardService.getRewardScores(courseId, currentUser.getId());
     }
 
     @MutationMapping
-    public RewardScores recalculateScores(@Argument UUID courseId, @Argument UUID userId) {
+    public RewardScores courseRecalculateScoresForUser(@Argument UUID courseId, @Argument UUID userId) {
         return rewardService.recalculateScores(courseId, userId);
     }
-
 }
